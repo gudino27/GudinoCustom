ebd407c1184b5fb17da1c94c9ca87849a005cf93
gudino27 <gudino27@icloud.com>
2026-02-05 13:02:25 -0800

    restarting from react native to swift then moving on to kotlin or simlar for android for a native application.

--- GCWadmin/metro.config.js
+++
@@ -1,6 +0,0 @@
-
-// Expo recommended metro config: extend expo/metro-config
-// Learn more: https://docs.expo.dev/guides/customizing-metro/
-const { getDefaultConfig } = require('expo/metro-config');
-
-module.exports = getDefaultConfig(__dirname);
--- GCWadmin/src/constants/index.js
+++
@@ -1,6 +0,0 @@
-// Export all design constants
-export { COLORS } from './colors';
-export { TYPOGRAPHY } from './typography';
-export { SPACING } from './spacing';
-export { RADIUS } from './radius';
-export { SHADOWS } from './shadows';
